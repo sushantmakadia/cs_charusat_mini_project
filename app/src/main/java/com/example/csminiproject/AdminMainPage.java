d89e2197d40a20acaede1ffe39f668616dec63aa
sushant makadia <makadiasushant@gmail.com>
2020-09-01 01:25:52 +0530

    added files

--- app/src/main/java/com/example/csminiproject/AdminMainPage.java
+++ app/src/main/java/com/example/csminiproject/AdminMainPage.java
@@ -6,15 +6,30 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminMainPage extends AppCompatActivity {
 CardView viewform ;
     CardView report ;
+    ImageView loagout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main_page);
         viewform = findViewById(R.id.card_requested);
+        loagout =findViewById(R.id.logout);
+        final FirebaseAuth fAuth = FirebaseAuth.getInstance();
+        loagout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                fAuth.signOut();
+                startActivity(new Intent(AdminMainPage.this, MainActivity.class));
+                finish();
+            }
+        });
         viewform.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
